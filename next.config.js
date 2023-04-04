b7dd9a45189df1884cee559183d38a9a5e15c2df
bxbdev <bxbdev2022@gmail.com>
2023-04-05 02:58:45 +0800

    updated

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   },
   images: {
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com']
+  },
+  externals: {
+    // Add any problematic dependencies here
+    'supports-color': 'supports-color'
   }
 }
 
